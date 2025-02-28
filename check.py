0ae9f60b1863b0abe727fc396c58e1f5eef3ee73
Rahil Shaikh <rahilshaikh7117@gmail.com>
2025-02-28 10:09:37 +0530

    tunneling

--- check.py
+++
@@ -1,5 +0,0 @@
-from pytube import YouTube
-url=input('Enter youtube video link: ')
-yt=YouTube(url,use_oauth=True,allow_oauth_cache=True)
-video=yt.streams.filter(only_audio=True).first()
-video.download()
